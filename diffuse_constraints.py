3912f56079bdfcfcd91f22ec8997acaf5a4b7366
Adam Coogan <dr.adam.coogan@gmail.com>
2019-04-23 16:49:11 +0200

    add documentation for diffuse helper functions

--- diffuse_constraints.py
+++ diffuse_constraints.py
@@ -4,25 +4,69 @@ from scipy.optimize import root_scalar
 from constants import kpc_to_cm, exp_tau
 from constants import hubble, dnde_interps, speed_of_light, rho_dm_avg_0
 from constants import e_egb, phi_egb, err_high_egb, r_s_mw, rho_einasto
-from constants import d_earth, pbh_ann_rate
+from constants import d_earth, pbh_ann_rate, fs_0
 
 
-# LOS integral away from GC
+"""
+Functions for computing constraints on PBHs with diffuse gamma-ray
+observations.
+"""
+
+
+# Integral of density along line-of-sight away from the galactic center.
 rho_los_int = quad(rho_einasto, d_earth, 1000*r_s_mw)[0]
 
 
-def phi_gal(e, m_dm, sv, m_pbh, f, fs="b"):
-    """Galactic flux from DM surrounding PBHs, assuming the PBHs trace the
-    galactic DM distribution. This is an irreducible contribution to the
-    isotropic gamma-ray flux. (GeV cm^2 sr s)^-1
+def phi_gal(e, m_dm, sv, m_pbh, f, fs=fs_0):
+    """Diffuse gamma-ray flux from PBHs along line-of-sight away from the
+    galactic center.
+
+    Parameters
+    ----------
+    e : float
+        Gamma-ray energy.
+    m_dm : float
+        DM mass.
+    sv : float
+        Self-annihilation cross section.
+    m_pbh : float
+        PBH mass.
+    f : float
+        Relative PBH abundance.
+    fs : str
+        DM annihilation final state.
+
+    Returns
+    -------
+    float
+        phi, (GeV cm^2 sr s)^{-1}.
     """
     return (f * pbh_ann_rate(m_dm, sv, m_pbh) / m_pbh * rho_los_int /
             (4 * np.pi) * dnde_interps[fs](e, m_dm).flatten()) / (kpc_to_cm)**2
 
 
-def phi_ex(e, m_dm, sv, m_pbh, f, fs="b"):
-    """Extragalactic flux from DM surrounding PBHs, assuming the PBHs make up a
-    fraction f of the DM. (GeV cm^2 sr s)^-1
+def phi_ex(e, m_dm, sv, m_pbh, f, fs=fs_0):
+    """Extragalactic flux from PBHs, obtained by integrating over redshifts.
+
+    Parameters
+    ----------
+    e : float
+        Gamma-ray energy.
+    m_dm : float
+        DM mass.
+    sv : float
+        Self-annihilation cross section.
+    m_pbh : float
+        PBH mass.
+    f : float
+        Relative PBH abundance.
+    fs : str
+        DM annihilation final state.
+
+    Returns
+    -------
+    float
+        phi, (GeV cm^2 sr s)^{-1}.
     """
     @np.vectorize
     def _phi_ex(e):
@@ -42,27 +86,68 @@ def phi_ex(e, m_dm, sv, m_pbh, f, fs="b"):
     return _phi_ex(e)
 
 
-def phi_diff(e, m_dm, sv, m_pbh, f, fs="b"):
-    """Total isotropic gamma-ray flux from DM around PBHs, (GeV cm^2 sr s)^-1.
+def phi_diff(e, m_dm, sv, m_pbh, f, fs=fs_0):
+    """Total isotropic gamma-ray flux from DM around PBHs.
+
+    Parameters
+    ----------
+    e : float
+        Gamma-ray energy.
+    m_dm : float
+        DM mass.
+    sv : float
+        Self-annihilation cross section.
+    m_pbh : float
+        PBH mass.
+    f : float
+        Relative PBH abundance.
+    fs : str
+        DM annihilation final state.
+
+    Returns
+    -------
+    float
+        phi, (GeV cm^2 sr s)^{-1}.
     """
     return (phi_gal(e, m_dm, sv, m_pbh, f, fs) +
             phi_ex(e, m_dm, sv, m_pbh, f, fs))
 
 
 @np.vectorize
-def sv_bound_diff(m_dm, m_pbh, f, n_sigma=3, source="ex"):
-    """Computes a quick-and-dirty conservative bound on <sigma v> by requiring
-    the diffuse flux from PBHs to exceed the measured value by less than
-    n_sigma standard deviations.
+def sv_bound_diff(m_dm, m_pbh, f, n_sigma=3, source="ex", fs=fs_0):
+    """Computes a quick-and-dirty bound on <sigma v> by requiring the diffuse
+    flux from PBHs to exceed the measured value by no more than n_sigma
+    standard deviations.
+
+    Parameters
+    ----------
+    m_dm : float
+        DM mass.
+    m_pbh : float
+        PBH mass.
+    f : float
+        Relative PBH abundance.
+    n_sigma : float
+        Significance required for PBH signal to be considered significant.
+    source : str
+        Which contributions of PBHs to the diffuse flux to consider: "ex" for
+        extragalactic only, "gal" for galactic only or "gal+ex" for both.
+    fs : str
+        DM annihilation final state.
+
+    Returns
+    -------
+    float
+        Bound on <sigma v>.
     """
     def objective(log10_sv):
         sv = 10**log10_sv
         if source == "ex":
-            phi_dm = phi_ex(e_egb, m_dm, sv, m_pbh, f)
+            phi_dm = phi_ex(e_egb, m_dm, sv, m_pbh, f, fs)
         elif source == "gal":
-            phi_dm = phi_gal(e_egb, m_dm, sv, m_pbh, f)
+            phi_dm = phi_gal(e_egb, m_dm, sv, m_pbh, f, fs)
         elif source == "gal+ex":
-            phi_dm = phi_diff(e_egb, m_dm, sv, m_pbh, f)
+            phi_dm = phi_diff(e_egb, m_dm, sv, m_pbh, f, fs)
         return np.max((phi_dm - phi_egb) / err_high_egb) - n_sigma
 
     sol = root_scalar(
