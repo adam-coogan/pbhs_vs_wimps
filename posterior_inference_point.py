d0784ea1e68e6bf8b9f15236df21560d7899bc4c
Adam Coogan <acoogan@ucsc.edu>
2019-04-23 21:15:02 +0200

    add documentation for PointSourcePosterior

--- posterior_inference_point.py
+++ posterior_inference_point.py
@@ -17,12 +17,44 @@ Classes for performing posterior analysis with point source constraints.
 p_gamma_dir = "data/p_gammas/"
 
 
-class Distribution_N_gamma:  # __init__(m_pbh), __call__(n_gamma, sv, f, m_dm)
-    """Represents p(n_gamma|m_pbh, f, m_dm, sv). Provides functions to save
-    tables containing the distribution's parameters.
+class Distribution_N_gamma:
     """
-    def __init__(self, m_pbh, fs=fs_0, flux_type=flux_type_0, b_cut=b_cut_0,
-                 flux_thresh=flux_thresh_0, n_samples=50000, test=True):
+    Represents p(n_gamma|m_pbh, f, m_dm, sv). Provides functions to save
+    tables containing the distribution's parameters, which are computed using
+    PBHHaloSim.
+    """
+
+    def __init__(self,
+                 m_pbh,
+                 fs=fs_0,
+                 flux_type=flux_type_0,
+                 b_cut=b_cut_0,
+                 flux_thresh=flux_thresh_0,
+                 n_samples=50000,
+                 test=True):
+        """Initializer.
+
+        Parameters
+        ----------
+        m_pbh : float
+            PBH mass.
+        fs : str
+            Annihilation final state.
+        flux_type : str
+            Use "dnde" to compute fluxes or "e dnde" to compute energy fluxes.
+        b_cut : float
+            Specifies the latitude (|b|) cut for calculating the number of
+            detectable PBH halos.
+        flux_thresh : float
+            Flux detectability threshold in cm^-2 s^-1 (for flux_type "dnde")
+            or erg cm^-2 s^-1 (for flux_type "e dnde").
+        n_samples : int
+            Number of iterations of the simulation to run.
+        test : bool
+            Setting this to True reads and writes all data tables to test/
+            subdirectories. This is useful when worried about overwriting large
+            tables that took a long time to compute.
+        """
         self.m_pbh = m_pbh
         self.fs = fs
         self.flux_type = flux_type
@@ -39,7 +71,7 @@ class Distribution_N_gamma:  # __init__(m_pbh), __call__(n_gamma, sv, f, m_dm)
 
             Returns
             -------
-            MC estimate of p_gamma and corresponding error.
+            MC estimate of p_gamma and its MC error.
             """
             sim = PBHHaloSim(
                 m_pbh=self.m_pbh,
@@ -71,19 +103,19 @@ class Distribution_N_gamma:  # __init__(m_pbh), __call__(n_gamma, sv, f, m_dm)
         if svs.size <= 1:
             raise ValueError("svs must have more than one element")
 
+        # Compute the table values
         sv_col = np.repeat(svs, m_dms.size)
         m_dm_col = np.tile(m_dms, svs.size)
-        # Compute the table values
         p_gammas, p_gamma_errs = self._get_p_gamma_val(m_dm_col, sv_col)
 
         # Save the data table
-        np.savetxt(
-            "{}{}p_gamma_M={:.1f}.csv".format(
-                p_gamma_dir, "test/" if self.test else "", self.m_pbh),
-            np.stack([sv_col, m_dm_col, p_gammas, p_gamma_errs]).T,
-            header=("p_gamma for M_PBH = {:.1f} M_sun.\n"
-                    "Columns are: <sigma v> (cm^3/s), m_DM (GeV), "
-                    "p_gamma, MC error.").format(self.m_pbh))
+        fname = "{}{}p_gamma_M={:.1f}.csv".format(
+            p_gamma_dir, "test/" if self.test else "", self.m_pbh)
+        np.savetxt(fname,
+                   np.stack([sv_col, m_dm_col, p_gammas, p_gamma_errs]).T,
+                   header=("p_gamma for M_PBH = {:.1f} M_sun.\n"
+                           "Columns are: <sigma v> (cm^3/s), m_DM (GeV), "
+                           "p_gamma, MC error.").format(self.m_pbh))
 
     def _load_p_gamma_table(self):
         """Loads interpolators mapping (m_dm, sv) to p_gamma and its error.
@@ -94,6 +126,7 @@ class Distribution_N_gamma:  # __init__(m_pbh), __call__(n_gamma, sv, f, m_dm)
         except OSError:
             raise Exception("p_gamma table not found. Construct it with "
                             "`save_p_gamma_table()`.")
+        # Package tables into RegularGridInterpolators
         self.svs = np.unique(sv_col)
         self.m_dms = np.unique(m_dm_col)
         p_gammas = p_gamma_col.reshape([self.svs.size, self.m_dms.size])
@@ -116,14 +149,23 @@ class Distribution_N_gamma:  # __init__(m_pbh), __call__(n_gamma, sv, f, m_dm)
         self.p_gamma_err = p_gamma_err
 
     def __call__(self, n_gamma, sv, f, m_dm):
-        """p(n_gamma|m_pbh, f, m_dm, sv)
+        """Evaluates the PDF.
 
         Parameters
         ----------
         n_gamma : int
             Number of PBHs detected as point sources by Fermi.
-        sv : float
+        sv : float or np.array
+            DM self-annihilation cross section.
         f : float
+            Relative PBH abundance.
+        m_dm : float or np.array
+            DM mass.
+
+        Returns
+        -------
+        np.array
+            p(n_gamma|m_pbh, f, m_dm, sv)
         """
         if self.p_gamma is None:
             self._load_p_gamma_table()
@@ -137,28 +179,41 @@ class Distribution_N_gamma:  # __init__(m_pbh), __call__(n_gamma, sv, f, m_dm)
     @m_pbh.setter
     def m_pbh(self, val):
         self._m_pbh = val
+        # Dump the current tables
         self.p_gamma = None
         self.p_gamma_err = None
 
 
-class Distribution_U:  # __init__(lambda_prior), __call__(n_gamma, n_u)
-    """Represents p(U|n_gamma)."""
+class Distribution_U:
+    """
+    Represents the unassociated source count probability p(n_u|n_gamma).
+    """
+
     def __init__(self, lambda_prior="LF"):
         """
         Parameters
         ----------
         prior : "LF", "U"
-            Specifies the prior for lambda. Defaults to the conservative
-            choice, "LF". The optimistic case is "U", and "J" is in-between.
+            Specifies the prior for lambda, the astrophysical fake rate.
+            Defaults to the conservative choice, "LF". The optimistic case is
+            "U", and "J" is in-between.
         """
         self.lambda_prior = lambda_prior
 
     def __call__(self, n_gamma, n_u):
-        """
+        """Evaluates the PDF.
+
         Parameters
         ----------
         n_gamma : int
+            Number of PBHs detectable as gamma-ray sources.
         n_u : int
+            Number of observed unassociated point sources.
+
+        Returns
+        -------
+        np.array
+            p(n_u|n_gamma).
         """
         def helper(n_gamma, n_u):
             if n_u > n_gamma:
@@ -171,6 +226,7 @@ class Distribution_U:  # __init__(lambda_prior), __call__(n_gamma, n_u)
                     return 1.
             else:
                 return 0
+
         return np.vectorize(helper)(n_gamma, n_u)
 
     @property
@@ -186,8 +242,20 @@ class Distribution_U:  # __init__(lambda_prior), __call__(n_gamma, n_u)
 
 
 class PointSourcePosterior(Posterior):
-    def __init__(self, m_pbh, n_pbh, n_u=n_u_0, merger_rate_prior="LF",
-                 lambda_prior="LF", sv_prior="U", fs=fs_0, test=True):
+    """
+    Class for performing posterior analysis with point-source constraints. See
+    the submission scripts and posterior_analysis_tutorial.ipynb for examples.
+    """
+
+    def __init__(self,
+                 m_pbh,
+                 n_pbh,
+                 n_u=n_u_0,
+                 merger_rate_prior="LF",
+                 lambda_prior="LF",
+                 sv_prior="U",
+                 fs=fs_0,
+                 test=True):
         """Initializer.
 
         Parameters
@@ -196,6 +264,8 @@ class PointSourcePosterior(Posterior):
             PBH mass.
         n_pbh : float
             Number of PBH detections
+        n_u : int
+            Number of observed unassociated point sources.
         merger_rate_prior : str
             Prior on R: either "LF" (log-flat, the conservative choice) or "J"
             (Jeffrey's).
@@ -221,9 +291,13 @@ class PointSourcePosterior(Posterior):
         self.lambda_prior = lambda_prior
 
     def save_p_gamma_table(self, m_dms, svs):
+        """Saves the current p_gamma table. Wraps
+        Distribution_N_gamma.save_p_gamma_table()."""
         self._p_n_gamma.save_p_gamma_table(m_dms, svs)
 
     def load_p_gamma_table(self):
+        """Loads p_gamma table for the current parameter set. Wraps
+        Distribution_N_gamma.load_p_gamma_table()."""
         self._p_n_gamma.load_p_gamma_table()
 
     def integrand(self, f, n_gamma, sv, m_dm):
@@ -231,42 +305,25 @@ class PointSourcePosterior(Posterior):
 
         Parameters
         ----------
-        sv : float
-        n_gamma : int
+        f : np.array
+            PBH relative abundance.
+        n_gamma : np.array
             Number of PBHs passing Fermi point source cuts.
-        f : float
+        sv : np.array
+            DM self-annihilation cross sections.
+        m_dm : np.array
+            DM masses.
+
+        Returns
+        -------
+        np.array
+            Float/summand value.
         """
         def helper(f, n_gamma, sv, m_dm):
             return (self.p_sv(sv) * self.p_f(f) * self.p_u(n_gamma) *
                     self.p_n_gamma(n_gamma, sv, f, m_dm))
 
         return np.vectorize(helper)(f, n_gamma, sv, m_dm)
-
-    def _get_f_quad_points(self, fs, integrand_vals, frac=0.1, n=10):
-        """Samples log-space points around the peak of the posterior integrand.
-
-        Returns
-        -------
-        Array of log-spaced values of f out to where the integrand decreases to
-        10% of its maximum value, and the f at which it attains its maximum
-        value.
-        """
-        integrand_max = integrand_vals.max(axis=0)
-        # Choose some fraction of the max value to sample out to
-        min_sample_val = frac * integrand_max
-        d = (np.sign(min_sample_val - integrand_vals[:-1]) -
-             np.sign(min_sample_val - integrand_vals[1:]))
-        try:
-            f_low = fs[max(0, np.where(d > 0)[0][0])]
-        except IndexError:
-            f_low = fs[0]
-        try:
-            f_high = fs[min(len(fs)-1, np.where(d < 0)[0][-1])]
-        except IndexError:
-            f_high = fs[-1]
-
-        return np.append(np.geomspace(f_low, f_high, n-1),
-                         fs[np.argmax(integrand_vals, axis=0)])
 
     def _get_trapz_f_samples(self, fs, integrand_vals, frac=0.1, n_low=50,
                              n_peak=225, n_high=125):
@@ -275,10 +332,11 @@ class PointSourcePosterior(Posterior):
 
         Parameters
         ----------
-        fs : np.array with shape N
-        integrand_vals : np.array with shape N x M
-            Array of integrand values, with rows corresponding to the values in
-            fs and columns to different values of n_gamma.
+        fs : np.array
+            Array of f values with shape [N].
+        integrand_vals : np.array
+            Array of integrand values with shape [N, M]. The rows correspond to
+            the values in fs and columns to different values of n_gamma.
         n_low : int
             Number of points to sample below peak. If peak is flush against
             fs[0], then n_low more values will be sampled around the peak.
@@ -290,8 +348,9 @@ class PointSourcePosterior(Posterior):
 
         Returns
         -------
-        np.array with shape n_low+n_peak+n_high x M
-            Resampled f values
+        np.array
+            Array of shape [n_low + n_peak + n_high, M] containing resampled f
+            values.
         """
         # Define the integrand's peak
         integrand_max = integrand_vals.max(axis=0)
@@ -312,14 +371,17 @@ class PointSourcePosterior(Posterior):
             n_peak_cur = n_peak
             f_samples_low = np.array([])
             f_samples_high = np.array([])
+
             if f_low[i] == fs[0]:  # peak is flush against fs[0]
                 n_peak_cur += n_low
             else:
                 f_samples_low = np.geomspace(fs[0], f_low[i], n_low)
+
             if f_high[i] == fs[-1]:  # peak is flush against fs[-1]
                 n_peak_cur += n_high
             else:
                 f_samples_high = np.geomspace(f_high[i], fs[-1], n_high)
+
             f_samples_peak = np.geomspace(f_low[i], f_high[i], n_peak_cur)
             f_samples[:, i] = np.concatenate(
                 [f_samples_low, f_samples_peak, f_samples_high])
@@ -328,9 +390,21 @@ class PointSourcePosterior(Posterior):
 
     def _get_posterior_val(self, sv, m_dm):
         """Computes the posterior for <sigma v>. Supports broadcasting over sv
-        and m_dm. See documentation for `posterior_integrand`.
+        and m_dm.
+
+        Parameters
+        ----------
+        sv : np.array
+            DM self-annihilation cross sections.
+        m_dm : np.array
+            DM masses.
+
+        Returns
+        -------
+        np.array
+            Posterior PDF at the specified point.
         """
-        def helper(sv, m_dm):
+        def helper(sv, m_dm):  # computes posterior at a single point
             # Compute integrand values over an initial f grid
             fs = np.geomspace(f_min, f_max, 100)
             n_gammas = np.arange(0, self.n_u + 1, 1)
@@ -343,17 +417,20 @@ class PointSourcePosterior(Posterior):
             integrand_vals = self.integrand(f_mg, n_gamma_mg, sv, m_dm)
 
             return trapz(integrand_vals, f_mg, axis=0).sum()
+
         return np.vectorize(helper)(sv, m_dm)
 
     def filename_suffix(self):
-        """Add extra info to filename"""
+        """Add extra info to filename."""
         return "ps_{}_prior_lambda={}".format(super().filename_suffix(),
                                               self.lambda_prior)
 
     def p_n_gamma(self, n_gamma, sv, f, m_dm):
+        """Computes p(n_gamma|m_pbh, f, m_dm, sv)."""
         return self._p_n_gamma(n_gamma, sv, f, m_dm)
 
     def p_u(self, n_gamma):
+        """Computes p(n_u|n_gamma)."""
         return self._p_u(n_gamma, self.n_u)
 
     @property
@@ -362,12 +439,13 @@ class PointSourcePosterior(Posterior):
 
     @fs.setter
     def fs(self, val):
+        # Synchronizes final state
         self._p_n_gamma.fs = val
         self._fs = val
 
     @Posterior.m_pbh.setter
     def m_pbh(self, val):
-        # Synchronize attribute
+        # Synchronize PBH mass
         Posterior.m_pbh.fset(self, val)
         self._p_n_gamma.m_pbh = val
 
@@ -377,6 +455,6 @@ class PointSourcePosterior(Posterior):
 
     @lambda_prior.setter
     def lambda_prior(self, val):
-        # Synchronize attribute
+        # Synchronize astrophysical fake rate prior
         self._lambda_prior = val
         self._p_u.lambda_prior = val
